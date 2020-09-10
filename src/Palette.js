040127294a4847ed617b3d19b378cb437d9aa07d
Giselle_Design <gh.saadat63@gmail.com>
2020-09-10 16:31:49 -0700

    create palette of color app

---
+++ src/Palette.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class Palette extends Component {
+    render(){
+        return (
+            <div className="Palette">
+                <div className="palette-colors">
+
+                </div>
+                
+            </div>
+        )
+    }
+   
+}
+
+export default Palette;
