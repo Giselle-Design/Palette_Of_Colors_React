2ab1477165797db3238fe5ce4a6925c8fbb2e845
Giselle_Design <gh.saadat63@gmail.com>
2020-09-11 15:32:07 -0700

    Add Navbar & Do some styling

--- src/Palette.js
+++ src/Palette.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 import "./Palette.css";
 
 
+
+
 class Palette extends Component {
-    render(){
-        const colorboxes = this.props.colors.map(color =>(
-            <ColorBox background={color.color} name={color.name} />    
+    constructor(props){
+        super(props);
+        this.state = { level: 500 };
+        this.ChangeLevel=this.ChangeLevel.bind(this)
+    }
+
+    ChangeLevel(level){
+        this.setState({level})
+    }
+
+    render() {
+             const { colors } = this.props.palette;
+             const { level } = this.state;
+             const colorboxes = colors[level].map(color =>(
+            <ColorBox background={color.hex} name={color.name} />    
                 ));
         return (
             <div className="Palette">
+                <Navbar level={level} ChangeLevel={this.ChangeLevel}/>
                 <div className="palette-colors">
                     {colorboxes}
                 </div>
