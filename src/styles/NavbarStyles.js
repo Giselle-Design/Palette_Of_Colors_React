6248fe1fa95804f38e32cf913785330177fea37a
Giselle_Design <gh.saadat63@gmail.com>
2020-10-07 10:26:06 -0700

    Make Navbar Responsive

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -1,69 +1,56 @@
-import sizes from "./sizes";
-export default {
 
-    Navbar: {
-        display: "flex",
-        alignContent: "center",
-        justifyContent: "flex-start",
-        height: "6vh",
-        
+import sizes from "./sizes";
+
+export default {
+  Navbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    height: "6vh"
+  },
+  logo: {
+    marginRight: "15px",
+    padding: "0 13px",
+    fontSize: "22px",
+    backgroundColor: "#eceff1",
+    fontFamily: "Satisfy",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    "& a": {
+      textDecoration: "none",
+      color: "black"
     },
-    
-    
-    logo: {
-        marginRight: "15px",
-        padding: "0 13px",
-        fontSize: "22px",
-        backgroundColor: "#eceff1",
-        fontFamily: "Satisfy",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        "& a": {
-            textDecoration: "none",
-            color: "red",
-        },
-        [sizes.down("xs")]: {
-            display: "none"
-          }
-    },
-    
-    
-    sliderContainer: {
-        marginTop: "18px",
-        marginLeft: "15px",
-        
-    },
-    
-    selectContainer: {
-        marginLeft: "auto",
-        marginTop: "15px",
-        marginRight: "1rem",
-    },
-    
-    slider: {
-        width: "340px",
-        margin: "0 25px",
-        display: "inline-block", 
-        "& .rc-slider-track": {
-            backgroundColor: "transparent"
-        }, 
-        "& .rc-slider-rail": {
-            height: "8px" 
-        },
-        "& .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus, .rc-slider-handle:hover": {
-            backgroundColor: "red",
-            outline: "none",
-            border: "2px solid red",
-            boxShadow: "none",
-            width: "13px",
-            height: "13px",
-            marginLeft: "-7px",
-            marginTop: "-3px",
-        },
-        
-        [sizes.down("sm")]: {
-            width: "150px"
-          }
+    [sizes.down("xs")]: {
+      display: "none"
     }
+  },
+  slider: {
+    width: "340px",
+    margin: "0 10px",
+    display: "inline-block",
+    "& .rc-slider-track": {
+      backgroundColor: "transparent"
+    },
+    "& .rc-slider-rail": {
+      height: "8px"
+    },
+    "& .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus,.rc-slider-handle:hover": {
+      backgroundColor: "red",
+      outline: "none",
+      border: "2px solid red",
+      boxShadow: "none",
+      width: "13px",
+      height: "13px",
+      marginLeft: "-1px",
+      marginTop: "-3px"
+    },
+    [sizes.down("sm")]: {
+      width: "150px"
+    }
+  },
+  selectContainer: {
+    marginLeft: "auto",
+    marginRight: "1rem"
+  }
 };
